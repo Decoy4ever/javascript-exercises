e05338e4a8af6611be3dc5e703401bd82a4e7fb3
DK <dzilla398@gmail.com>
2024-01-29 10:50:18 +1030

    completed getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrOfBooks) {
+    return arrOfBooks.map(book => `${book.title}`);
+    // console.log(result)
 };
 
 // Do not edit below this line
